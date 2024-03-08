9b620584d4c5c0693fdccb1b7a8728b5247c32a3
Akash Chakurte <akash.chakurte@pay1.in>
2024-03-08 11:09:46 +0530

    8 feb

--- src/test/java/com/spotify/oauth2/api/SpecBuilder.java
+++ src/test/java/com/spotify/oauth2/api/SpecBuilder.java
@@ -20,6 +20,7 @@ public class SpecBuilder {
                 setContentType(ContentType.JSON).
                 addFilter(new AllureRestAssured()).
                 log(LogDetail.ALL).build();
+        //line for jenkins
     }
 
     public static RequestSpecification getAccountRequestSpec(){
